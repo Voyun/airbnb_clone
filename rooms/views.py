fd79055e8fd7b8aeea7aa2f773796aedc4fbaaaf
Voyun <voyun0819@gmail.com>
2021-01-21 00:33:35 +0900

    update

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,77 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 
-def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+def search(request):
+    print(request)
+    return render(request, "rooms/search.html")
+
+
+""" def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+
+    except models.Room.DoesNotExist:
+        raise Http404() """
+
+
+"""   def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context """
+
+
+"""def all_rooms(request):
+
+    Pagination with Limit and Offset
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
+    )
+
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"rooms": rooms})
+    except EmptyPage:
+        return redirect("/")
+    """
